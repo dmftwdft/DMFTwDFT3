87988051e2e0649dbca53928a967a1d684eeeda8
Uthpala Herath <uthpala.herath@duke.edu>
2026-07-11 20:51:02 -0400

    Updated cite info in spash screen

--- bin/splash.py
+++ bin/splash.py
@@ -19,14 +19,14 @@ def welcome():
     print(art)
     print(("Python 3 version running on Python %s." % pversion))
     print(
-        "\n--- An open-source code combining Dynamical Mean Field Theory with various Density Functional Theory packages. ---"
+        "\n--- An open-source framework to treat strongly correlated materials using DMFT ---"
     )
     print(("\nVersion %s updated on %s.\n" % (version, date)))
     print(
         "Please cite:\n \
 Vijay Singh, Uthpala Herath, Benny Wah, Xingyu Liao, Aldo H. Romero, Hyowon Park.,\n \
 DMFTwDFT: An open-source code combining Dynamical Mean Field Theory with various Density Functional Theory packages.,\n \
-Computer Physics Communications 261 (2021):107778.\n"
+Computer Physics Communications 261, 107778 (2021). https://doi.org/10.1016/j.cpc.2020.107778\n"
     )
 
     separator_art = """
